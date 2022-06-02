0aca9b31ed029194cd722a55d57095053da76db3
Felipe <machadofelipe2016@outlook.com>
2022-06-02 14:56:05 -0300

    c11- adding all name of components inside authProvider

--- newproject/src/App.js
+++ newproject/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import React from 'react'
+import { AuthProvider } from './manegecontext/auth';
+import Comments from './Userall/Comments';
+import CreateUser from './Userall/CreateUser';
+import NewComents from './Userall/Newcomments';
+import SingUserSin from './Userall/SingUser';
+import UserInfo from './Userall/UserInfo';
+
 
 function App() {
-  return (
-    <div className="App">
-     <h1 className='bg-primary text-center'>Testing here bootstrap</h1>
-    </div>
+  return ( <AuthProvider>
+     <div className='d-flex text-center' >
+      <NewComents/>
+      <CreateUser/>
+      <Comments/>
+      <SingUserSin/>
+      <UserInfo/>
+     </div>
+    </AuthProvider>
   );
 }
 
