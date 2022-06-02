741a5a6ea8030ca5882578cd61a510270c02d396
Felipe <machadofelipe2016@outlook.com>
2022-06-02 14:52:08 -0300

    c4-create all part of singuser

---
+++ newproject/src/Userall/SingUser.js
@@ -0,0 +1,36 @@
+import React, {useState, useContext} from 'react'
+import { AuthContext } from '../manegecontext/auth'
+
+
+const SingUserSin = () => {
+    const auth = useContext(AuthContext)
+    const [form, setform] =useState({
+        email:'', password:''
+    })
+     
+    const Onchange = fielt => evt =>{
+        setform({
+            ...form,
+            [fielt]: evt.target.value
+        })
+    }
+    if(auth.users !== null){
+        return null
+    }
+
+    console.log('testing here place' ,auth.users)
+    
+    return(<div className=' d-flex text-center bg-primary '>
+           <h1 className='text-center'> Sing in Account:</h1>
+           
+            {auth.singUserInside.SingCreateState.error !== '' && 
+            <p> {auth.singUserInside.SingCreateState.error} </p> }
+           
+           <input value={form.email} onChange={Onchange('email')} placeholder='Write your email:'  />
+           <input value={form.password} onChange={Onchange('password')} />
+           <button className='bg-dark' onClick={() => auth.singUserInside.singUserInside(form.email, form.password) } >Sing In</button>
+
+    </div>)
+}
+
+export default SingUserSin
