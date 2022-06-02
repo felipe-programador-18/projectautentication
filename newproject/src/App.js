51f52977e7209d0f2bd3203d7d76725e2be58ab0
Felipe <machadofelipe2016@outlook.com>
2022-06-02 19:36:22 -0300

    c25-make some test inside code

--- newproject/src/App.js
+++ newproject/src/App.js
@@ -1,20 +1,21 @@
 import './App.css';
 import React from 'react'
 import { AuthProvider } from './manegecontext/auth';
-import Comments from './Userall/Comments';
-import CreateUser from './Userall/CreateUser';
+//import Comments from './Userall/Comments';
+//import CreateUser from './Userall/CreateUser';
 import NewComents from './Userall/Newcomments';
-import SingUserSin from './Userall/SingUser';
+//testing about singuser with router
 import UserInfo from './Userall/UserInfo';
+import SingUserSin from './Userall/SingUser';
 
 
 function App() {
   return ( <AuthProvider>
      <div className='d-flex text-center' >
       <NewComents/>
-      <CreateUser/>
-      <Comments/>
+    
       <SingUserSin/>
+     
       <UserInfo/>
      </div>
     </AuthProvider>
