980e8917556c78da73c1c9afe7113b9564e43858
Felipe <machadofelipe2016@outlook.com>
2022-06-02 14:53:05 -0300

    c6-create all funcion that userinfo

---
+++ newproject/src/Userall/UserInfo.js
@@ -0,0 +1,41 @@
+import React, {useState,useContext} from 'react'
+import { AuthContext } from '../manegecontext/auth'
+
+const FormDisplay = ({displayName, users}) => {
+    const [ newdisplayName , setdisplayName] = useState(displayName)
+
+    const Onchange = evt => {
+        setdisplayName(evt.target.value)
+    }
+    // to save new users
+    const save = () => {
+        if(newdisplayName !== ''){
+            users.updateProfile({displayName: newdisplayName})
+        }
+    }
+    return(<>
+        <input type='text' value={newdisplayName} onChange={Onchange}  />
+        <button onClick={save} >Save New Name</button>
+    </>)
+}
+
+const UserInfo = () =>{
+const auth = useContext(AuthContext)
+
+if(auth.users === null){
+    return null
+}
+
+const {displayName} = auth.users
+const [alternativeDisplayName] = auth.users.email.split('@')
+const {dn} = displayName || alternativeDisplayName
+
+
+  return(<div>
+        <h1> Hi {dn}</h1>
+        <FormDisplay displayName={dn} users={auth.users} />
+         <button onClick={auth.singout} >Sing out </button>
+  </div>)
+}
+
+export default UserInfo
