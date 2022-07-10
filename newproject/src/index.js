ab12238bf1f8c47e9cf2f41527c83a52990f00c4
Felipe <machadofelipe2016@outlook.com>
2022-07-10 14:52:47 -0300

    c32-adding more routes

--- newproject/src/index.js
+++ newproject/src/index.js
@@ -5,17 +5,11 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { Routes, Route } from 'react-router-dom';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />} />
-      </Routes>
-    
-    </BrowserRouter>
+   <App />
   </React.StrictMode>
 );
 
