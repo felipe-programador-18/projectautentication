db0fd6053f5736ffe098edeaf7fac11563ad8e48
Felipe <machadofelipe2016@outlook.com>
2022-07-10 14:54:41 -0300

    c35-adding more coed

--- newproject/src/App.js
+++ newproject/src/App.js
@@ -3,11 +3,12 @@ import React from 'react'
 import { AuthProvider } from './manegecontext/auth';
 //import Comments from './Userall/Comments';
 import CreateUser from './Userall/CreateUser';
-import NewComents from './Userall/Newcomments';
 //testing about singuser with router
 import UserInfo from './Userall/UserInfo';
 import SingUserSin from './Userall/SingUser';
-import {Routes, Route} from 'react-router-dom'
+import { BrowserRouter ,Routes, Route} from 'react-router-dom'
+import Comments from './Userall/Comments';
+import Comment from './Userall/Comment';
 
 //<NewComents/>
 //<UserInfo/>
@@ -15,12 +16,15 @@ import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return ( <AuthProvider>
+    <BrowserRouter>
      <div className='d-flex text-center' >
        <Routes>
          <Route  path={'/'} element={<SingUserSin/>}  />
-          <Route  path= {'/CreateUser'} element={<CreateUser/>}  />
+         <Route  path= {'/CreateUser'} element={<CreateUser/>}  />
+         <Route path={'/comment'} element={<Comment/>}  />
         </Routes>
      </div>
+     </BrowserRouter>
     </AuthProvider>
   );
 }
